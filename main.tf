de5eba87ddfd09c5934a6e5c3094e7078d9d8b18
MMeerim <56653489+MMeerim@users.noreply.github.com>
2021-04-15 15:10:02 -0500

    add_keypair

--- main.tf
+++ main.tf
@@ -11,8 +11,11 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  access_key = "aws_access_key_id"
-  secret_key = "aws_secret_access_key"
+}
+
+resource "aws_key_pair" "ec2-user-public" {
+  key_name   = var.my_key_name  
+  public_key = var.my_publickey
 }
 
 resource "aws_eip" "for_each" {
