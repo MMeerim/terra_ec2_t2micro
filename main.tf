b6030a68bf2fd9584aa9d4aac3b9050388fa539d
MMeerim <56653489+MMeerim@users.noreply.github.com>
2021-04-15 15:16:13 -0500

    remove key

--- main.tf
+++ main.tf
@@ -13,11 +13,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_key_pair" "ec2-user-public" {
-  key_name   = var.my_key_name  
-  public_key = var.my_publickey
-}
-
 resource "aws_eip" "for_each" {
   count    = var.eip_count
   vpc      = var.vpc_bool
